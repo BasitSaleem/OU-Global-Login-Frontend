ade73b78f39761ae27380f458f59d192c5892099
maazkhurshid60 <maaz.khurshid60@gmail.com>
2025-12-08 15:22:50 +0500

    invite module done pushed for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Suspense } from 'react';
 import DashboardLayout from '@/components/layout/dashboard-layout';
 import HomePage from '@/components/pages/Homepage/home-page';
 import { ErrorBoundary } from '@/components/error-boundary';
-import { AuthGuard } from '@/components/HOCs/auth-guard';
 import { GlobalLoading } from '@/components/ui/loading';
 import { CreateOrganizationGuard } from '@/components/HOCs/createOrgRoute.guard';
 
