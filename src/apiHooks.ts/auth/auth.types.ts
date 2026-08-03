74007028e1791977ab783a54cf5c92fef610f1b6
DESKTOP-FLBA1Q7\Thinkpad <maaz.khurshid60@gmail.com>
2026-08-03 17:51:41 +0500

    - fixed signin bug changes.

--- src/apiHooks.ts/auth/auth.types.ts
+++ src/apiHooks.ts/auth/auth.types.ts
@@ -16,7 +16,7 @@ export interface signUpData {
   last_name: string;
   email: string;
   password: string;
-  acceptTerms: boolean;
+  acceptTerms?: boolean;
 }
 export interface VerifyOtpData {
   email: string;
